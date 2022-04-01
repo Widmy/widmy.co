45f99000fed7a7d066546aa3eba53b88dfc7cd32
Camilo Salinas <iam@camilosalinas.me>
2022-04-01 08:48:12 -0500

    Netlify Form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
+import { useState } from "react";
 import "./App.scss";
 import Intro from "./assets/intro.svg";
 function App() {
+  const [email, setEmail] = useState("");
+  const encode = (data) => {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+      )
+      .join("&");
+  };
+  const handleChange = (e) => setEmail(e.target.value);
+  const handleSubmit = (e) => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", email }),
+    })
+      .then(() => {
+        console.log("enviado");
+      })
+      .catch((error) => console.error(error, "error"));
+
+    e.preventDefault();
+  };
   return (
     <div className="main">
       <div className="bg"></div>
@@ -18,17 +41,20 @@ function App() {
         información de tus pacientes de manera segura.
       </p>
 
-      <div className="formulario">
+      <form className="formulario" onSubmit={handleSubmit}>
         <input
           type="email"
-          maxlength="256"
+          maxLength="256"
           name="email"
           data-name="email"
           placeholder="Ingresa tu email"
           id="email"
+          value={email}
+          onChange={handleChange}
         />
-        <button>Únete a la lista de espera</button>
-      </div>
+
+        <button type="submit">Únete a la lista de espera</button>
+      </form>
     </div>
   );
 }
