a392eab4353b04596fa57b46d0d61e1d9d1561d6
Camilo Salinas <iam@camilosalinas.me>
2022-08-03 07:44:44 -0500

    fix deploy

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -54,32 +54,32 @@ const features = [
     icon: LibraryIcon,
   },
 ];
-const logos = [
-  {
-    name: "Transistor",
-    url: "https://tailwindui.com/img/logos/transistor-logo-gray-400.svg",
-  },
-  {
-    name: "Mirage",
-    url: "https://tailwindui.com/img/logos/mirage-logo-gray-400.svg",
-  },
-  {
-    name: "Tuple",
-    url: "https://tailwindui.com/img/logos/tuple-logo-gray-400.svg",
-  },
-  {
-    name: "Laravel",
-    url: "https://tailwindui.com/img/logos/laravel-logo-gray-400.svg",
-  },
-  {
-    name: "StaticKit",
-    url: "https://tailwindui.com/img/logos/statickit-logo-gray-400.svg",
-  },
-  {
-    name: "Workcation",
-    url: "https://tailwindui.com/img/logos/workcation-logo-gray-400.svg",
-  },
-];
+// const logos = [
+//   {
+//     name: "Transistor",
+//     url: "https://tailwindui.com/img/logos/transistor-logo-gray-400.svg",
+//   },
+//   {
+//     name: "Mirage",
+//     url: "https://tailwindui.com/img/logos/mirage-logo-gray-400.svg",
+//   },
+//   {
+//     name: "Tuple",
+//     url: "https://tailwindui.com/img/logos/tuple-logo-gray-400.svg",
+//   },
+//   {
+//     name: "Laravel",
+//     url: "https://tailwindui.com/img/logos/laravel-logo-gray-400.svg",
+//   },
+//   {
+//     name: "StaticKit",
+//     url: "https://tailwindui.com/img/logos/statickit-logo-gray-400.svg",
+//   },
+//   {
+//     name: "Workcation",
+//     url: "https://tailwindui.com/img/logos/workcation-logo-gray-400.svg",
+//   },
+// ];
 const footerNavigation = {
   main: [
     { name: "Inicio", href: "#" },
